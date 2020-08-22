5f4fa606a1d6e3c86f779e7c85bb6e30f983137e
fergusthompson <fergus.thompson@hotmail.com>
2020-08-22 19:17:52 +0000

    connected mongo

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'my_portfolio'
+app.config["MONGO_URI"] = 'mongodb+srv://Fergus:f3rgu5@myfirstcluster.y70hl.mongodb.net/my_portfolio?retryWrites=true&w=majority'
+
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello world'
+@app.route('/get_skills')
+def get_skills():
+    return render_template("skills.html", skills=mongo.db.skills.find())
 
 
 if __name__ == '__main__':
