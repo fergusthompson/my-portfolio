738fd753f6bf61ec572fde8ab7872a8cd7b9f44e
fergusthompson <fergus.thompson@hotmail.com>
2020-08-25 13:45:24 +0000

    added register template

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -34,6 +35,11 @@ def contact():
 @app.route('/get_skills')
 def get_skills():
     return render_template("skills.html", skills=mongo.db.skills.find())
+
+
+@app.route('/admin_login', methods={'GET', 'POST'})
+def admin_login():
+    return render_template("admin_login.html")
 
 
 if __name__ == '__main__':
